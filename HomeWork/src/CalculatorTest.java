55ce8d01032c1bfa36d4186e0012f9e1680d4a7d
Damir <damir20020711@mail.ru>
2021-09-12 23:00:17 +0300

    Update project

---
+++ HomeWork/src/CalculatorTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+
+    @Test
+    public void calculate() {
+    }
+}
---
+++ HomeWork/src/ParserTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ParserTest {
+
+    @Test
+    public void parseOperatorOrError() {
+    }
+
+    @Test
+    public void parseIntOrError() {
+    }
+}
