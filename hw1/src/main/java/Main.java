98372fdb9f8db706af03e4a19f81f94bc32db100
Damir <damir20020711@mail.ru>
2021-09-14 13:21:10 +0300

    14.09

---
+++ hw1/src/main/java/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        if (args.length < 3) {
+            System.out.println("Args format is: val1 +-/* val2");
+            System.exit(1);
+        }
+
+        var val1 = Parser.parseIntOrError(args[0]);
+        var val2 = Parser.parseIntOrError(args[2]);
+        var operator = (Parser.parseOperatorOrError(args[1]));
+
+        System.out.println("Result is " + Calculator.calculate(val1, operator, val2));
+    }
+}
