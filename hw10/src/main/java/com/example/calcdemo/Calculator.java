b7cc2fd982c4dd6092e0e968a6a13e93cdcda395
Damir <damir20020711@mail.ru>
2021-12-27 02:53:19 +0300

    HW2

--- hw10/src/main/java/com/example/calcdemo/Calculator.java
+++
@@ -1,5 +0,0 @@
-package com.example.calcdemo;
-
-public interface Calculator {
-    String calculate(double a, double b, String operator);
-}
--- hw10/src/main/java/com/example/calcdemo/CalculatorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.calcdemo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CalculatorRepository extends JpaRepository<Cache,Integer> {
-    Cache findByFirstArgumentAndSecondArgumentAndOperator(String firstArgument, String secondArgument, String operator);
-}
